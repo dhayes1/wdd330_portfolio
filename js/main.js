99e89ceccdbc81f5e357d771bc3ef0087b289acb
donny.hayes <59512056+dhayes1@users.noreply.github.com>
2021-09-14 23:22:46 -0700

    dynamic list working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 // create table of contents list array
-const data = ["Ram", "Dodge", "Ford"];
-
-// get the contents list tag
-let ol = document.getElementById('content-list');
-
-// create a list item for each item in the contents list arry
-data.forEach((item) => {
-    let li = document.createElement("li");   //create list element
-    li.innerText - item;
-    ol.appendChild(li);
-});
+const data = [
+    {
+      label: "Week 01",
+      url: "week1/"
+    },
+    {
+      label: "Week 02",
+      url: "week2/"
+     }
+  ];
+  
+  let ol = document.getElementById('content-list');
+  
+  // create a list item for each item in the contents list arry
+  data.forEach((item) => {
+      let li = document.createElement("li");   //create list element
+      let a = document.createElement("a");
+      a.innerText = item.label;
+      a.href = item.url;
+      li.appendChild(a);
+      ol.appendChild(li);
+  });
