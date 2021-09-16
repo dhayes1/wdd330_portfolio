fbd67cea762aa6fad9d448389c9ae8b2b821d4b5
donny.hayes <59512056+dhayes1@users.noreply.github.com>
2021-09-15 22:12:48 -0700

    add comments

--- js/main.js
+++ js/main.js
@@ -2,22 +2,24 @@
 const data = [
     {
       label: "Week 01",
-      url: "week1/"
+      url: "week01/"
     },
-    {
+    // must include new objects in curly brackets
+    {                       
       label: "Week 02",
-      url: "week2/"
+      url: "week02/"
      }
   ];
   
+  // get ol tag for list (must be loaded or will cause errors)
   let ol = document.getElementById('content-list');
   
   // create a list item for each item in the contents list arry
   data.forEach((item) => {
-      let li = document.createElement("li");   //create list element
-      let a = document.createElement("a");
-      a.innerText = item.label;
-      a.href = item.url;
-      li.appendChild(a);
-      ol.appendChild(li);
+      let li = document.createElement("li");    // create list element
+      let a = document.createElement("a");      // creat hyperlink element
+      a.innerText = item.label;                 // set innertext value
+      a.href = item.url;                        // set href value
+      li.appendChild(a);                        // add hyperlink element to list element
+      ol.appendChild(li);                       // add list element(s) to ordered list element
   });
