8af87199a61a4237ca7120182ec43e9831f7e0ad
donny.hayes <59512056+dhayes1@users.noreply.github.com>
2021-10-09 22:58:55 -0700

    add week 4

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const data = [
     {
       label: "Week 03",
       url: "week03/index.html"
+    },
+    {
+      label: "Week 04",
+      url: "week04/index.html"
     }
     // must include new objects in curly brackets
     /*{                       
