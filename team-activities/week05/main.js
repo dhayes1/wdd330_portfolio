da4efb2819cb395c71ca92000f73da27eececbe8
donny.hayes <59512056+dhayes1@users.noreply.github.com>
2021-10-17 01:21:38 -0700

    update

--- team-activities/week05/main.js
+++ team-activities/week05/main.js
@@ -55,21 +55,58 @@ const hikeList = [
       parent.appendChild(renderOneHike(hike));
     });
   }
+
   function renderOneHike(hike) {
-    const item = document.createElement("li");
-  
-    item.innerHTML = ` <h2>${hike.name}</h2>
-          <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-          <div>
-                  <div>
-                      <h3>Distance</h3>
-                      <p>${hike.distance}</p>
-                  </div>
-                  <div>
-                      <h3>Difficulty</h3>
-                      <p>${hike.difficulty}</p>
-                  </div>
-          </div>`;
-  
-    return item;
+  const item = document.createElement("li");
+
+  item.innerHTML = ` <h2>${hike.name}</h2>
+        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+        <div>
+                <div>
+                    <h3>Distance</h3>
+                    <p>${hike.distance}</p>
+                </div>
+                <div>
+                    <h3>Difficulty</h3>
+                    <p>${hike.difficulty}</p>
+                </div>
+        </div>`;
+
+    item.addEventListener('click', () => {
+        //console.log("click");
+        getHikeByName(hike)
+    }); 
+
+  return item;
+}
+
+function getHikeByName(hikeData) {
+    const hikeListElement = document.getElementById("hikes");
+
+    hikeListElement.innerHTML =`<div>
+        <h2>${hikeData.name}</h2>
+        <div class="image">
+            <img src="${imgBasePath}${hikeData.imgSrc}" alt="${hikeData.imgAlt}">
+        </div>
+        <div>
+            <h3>Description:</h3>
+            <p>${hikeData.description}</p>
+        </div>
+        <div>
+            <h3>Distance:</h3>
+            <p>${hikeData.distance}</p>
+        </div>
+        <div>
+            <h3>Difficulty:</h3>
+            <p>${hikeData.difficulty}</p>
+        </div>
+        <div>
+            <h3>Directions:</h3>
+            <p>${hikeData.directions}</p>
+        </div>
+        <button class="BackButton">Return to List</button>
+    </div>`;
+
+    document.querySelector('.BackButton').addEventListener('click', showHikeList);
+
   }
